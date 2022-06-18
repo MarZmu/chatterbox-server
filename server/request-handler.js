9a40ac3587aa912dddb579f272e5c4def610225e
Marshall Zmuda <marshall.z@outlook.com>
2022-06-17 19:13:57 -0700

    pass first four tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,62 +11,95 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
-
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // The outgoing status.
-  var statusCode = 200;
-
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end('Hello, World!');
-};
-
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept, authorization',
   'access-control-max-age': 10 // Seconds.
 };
+
+var requestHandler = function (request, response) {
+  //how were going to handle the request
+  //declare variables for the request header (incoming information)
+  var url = request.url;
+  var path = request.path;
+
+
+  //declare the variables for the response variable (outgoing information)
+  var message = 'Hello Kitty!';
+  var statusCode;
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = 'application/json';
+
+  //what type of method it is
+  ////create a conditional statement for each
+
+  var testJSON = JSON.stringify([
+    {
+      username: 'bobby', message: 'HEYY whats up'
+    },
+    {
+      username: 'barbara', message: 'YOOOOO'
+    }
+  ]);
+  var ourPaths = ['/classes/messages'];
+
+  // console.log(testJSON, url, statusCode);
+
+  //modify the variable values
+  if (request.method === 'GET') {
+    // get retrives data
+    if (request.url.indexOf('classes/messages' !== -1)) {
+      //if matching path and method (success)
+      statusCode = 200;
+      headers['Content-Type'] = 'application/json';
+      message = testJSON;
+    } else {
+      //if wrong path (FAIL)
+      statusCode = 404;
+      message = 'Error: Path Not Found';
+    }
+  } else if (request.method === 'POST') {
+    //if matching path and method (success)
+    if (request.url.indexOf('classes/messages' !== -1)) {
+      //identify path, and send the data there
+
+      statusCode = 201;
+      message = 'Your Post is Successful!';
+    } else {
+      //if wrong path (FAIL)
+      statusCode = 404;
+      message = '2Error: Path Not Found';
+    }
+  } else if (request.method === 'PUT') {
+    // Updates the data
+
+
+  } else if (request.method === 'DELETE') {
+    // Delete the data
+
+  } else if (request.method === 'OPTIONS') {
+    // send back the methods we take
+    // or any other information about the server
+
+  }
+
+  console.log('Serving request type ' + request.method + ' for url ' + request.url + statusCode);
+
+
+
+  // .writeHead() writes to the request line and headers of the response,
+  // which includes the status and all headers.
+
+  //combine those variables into a response
+  response.writeHead(statusCode, headers);
+
+  // console.log(testJSON, statusCode);
+  // Calling .end "flushes" the response's internal buffer, forcing
+  // node to actually send all the data over to the client.
+  response.end(message);
+
+
+};
+
+module.exports.requestHandler = requestHandler;
